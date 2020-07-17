29e379a6241312957e356cd5cbffd752ae4dca21
flyfish11 <18314487191@163.com>
2020-07-17 11:15:48 +0800

    用户多次登录失败，锁定账户

---
+++ oauth-center/src/main/java/com/cloud/oauth/oauth2/AuthenticationFailureListener.java
@@ -0,0 +1,23 @@
+package com.cloud.oauth.oauth2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFailureListener implements ApplicationListener<AuthenticationFailureBadCredentialsEvent>
+{
+
+   @Autowired
+   private LoginAttemptService loginAttemptService;
+
+    @Override
+    public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent authenticationFailureBadCredentialsEvent) {
+        String account = authenticationFailureBadCredentialsEvent.getAuthentication().getPrincipal().toString();
+        loginAttemptService.loginFailed(account);
+
+        }
+
+}
+
